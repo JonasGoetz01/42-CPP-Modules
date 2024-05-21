adbdb5ece88bf53cadea83c2d858f13d8b8c4b01
Jonas Götz <jonas.goetz@egotec.com>
2024-05-21 10:31:23 +0200

    add contect function in phone book

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -9,3 +9,33 @@ PhoneBook::PhoneBook()
 PhoneBook::~PhoneBook()
 {
 }
+
+void PhoneBook::add()
+{
+	std::string firstName;
+	std::string lastName;
+	std::string nickname;
+	std::string phoneNumber;
+	std::string darkestSecret;
+	std::cout << "Enter first name: ";
+	std::getline(std::cin, firstName);
+	std::cout << "Enter last name: ";
+	std::getline(std::cin, lastName);
+	std::cout << "Enter nickname: ";
+	std::getline(std::cin, nickname);
+	std::cout << "Enter phone number: ";
+	std::getline(std::cin, phoneNumber);
+	std::cout << "Enter darkest secret: ";
+	std::getline(std::cin, darkestSecret);
+	this->contacts[this->contactIndex].setFirstName(firstName);
+	this->contacts[this->contactIndex].setLastName(lastName);
+	this->contacts[this->contactIndex].setNickname(nickname);
+	this->contacts[this->contactIndex].setPhoneNumber(phoneNumber);
+	this->contacts[this->contactIndex].setDarkestSecret(darkestSecret);
+	this->contactCount++;
+	this->contactIndex++;
+	if (this->contactIndex == 8)
+	{
+		this->contactIndex = 0;
+	}
+}
