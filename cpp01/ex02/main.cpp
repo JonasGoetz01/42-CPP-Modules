7690d87fc6da8d269482495b62101b0bf50be9e2
Jonas Götz <jonas.goetz01@web.de>
2024-05-22 13:30:31 +0200

    nicer messages

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string str = "HI THIS IS BRAIN";
-    std::string *stringPTR = &str;
-    std::string &stringREF = str;
-
-    // Printing memory addresses
-    std::cout << "Memory address of the string variable: " << &str << std::endl;
-    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
-    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
-
-    // Printing values
-    std::cout << "Value of the string variable: " << str << std::endl;
-    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
-    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
-
-    return 0;
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	// Printing memory addresses
+	std::cout << "Memory address with str var: " << &str << std::endl;
+	std::cout << "Memory address with ptr: " << stringPTR << std::endl;
+	std::cout << "Memory address with ref: " << &stringREF << std::endl;
+	// Printing values
+	std::cout << "Value of var: " << str << std::endl;
+	std::cout << "Value from ptr: " << *stringPTR << std::endl;
+	std::cout << "Value from ref: " << stringREF << std::endl;
+	return (0);
 }
