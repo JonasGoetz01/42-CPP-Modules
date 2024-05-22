36ad15992ab0e4ea53b1d0afeb010f8512ae0655
Jonas Götz <jonas.goetz01@web.de>
2024-05-22 16:49:45 +0200

    better testcases

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -59,6 +59,42 @@ int main( void ) {
             std::cout << "The point is outside the triangle." << std::endl;
         }
     }
+	{
+        Point a(Fixed(0), Fixed(0));
+        Point b(Fixed(0), Fixed(3));
+        Point c(Fixed(3), Fixed(0));
+        Point d(Fixed(0), Fixed(1));
+        Point point(d);
+
+        std::cout << "a: " << a.getX() << ", " << a.getY() << std::endl;
+        std::cout << "b: " << b.getX() << ", " << b.getY() << std::endl;
+        std::cout << "c: " << c.getX() << ", " << c.getY() << std::endl;
+        std::cout << "point: " << point.getX() << ", " << point.getY() << std::endl;
+
+        if (bsp(a, b, c, point)) {
+            std::cout << "The point is inside the triangle." << std::endl;
+        } else {
+            std::cout << "The point is outside the triangle." << std::endl;
+        }
+    }
+    {
+        Point a(Fixed(0), Fixed(0));
+        Point b(Fixed(0), Fixed(3));
+        Point c(Fixed(3), Fixed(0));
+        Point d(Fixed(0.1f), Fixed(1));
+        Point point(d);
+
+        std::cout << "a: " << a.getX() << ", " << a.getY() << std::endl;
+        std::cout << "b: " << b.getX() << ", " << b.getY() << std::endl;
+        std::cout << "c: " << c.getX() << ", " << c.getY() << std::endl;
+        std::cout << "point: " << point.getX() << ", " << point.getY() << std::endl;
+
+        if (bsp(a, b, c, point)) {
+            std::cout << "The point is inside the triangle." << std::endl;
+        } else {
+            std::cout << "The point is outside the triangle." << std::endl;
+        }
+    }
 
     return 0;
 }
