4598e120660808c8844e7f3b5eff269c3599422a
Jonas Götz <jonas.goetz01@web.de>
2024-05-22 16:43:21 +0200

    more testcases for all functions

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -1,16 +1,32 @@
+#include "Fixed.hpp"
 #include <iostream>
 
-#include "Fixed.hpp"
+int	main(void)
+{
+	Fixed	a;
 
-int main( void ) {
-    Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max( a, b ) << std::endl;
-    return 0;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << Fixed::min(a, b) << std::endl;
+	std::cout << (a > b) << std::endl;
+	std::cout << (a < b) << std::endl;
+	std::cout << (a >= b) << std::endl;
+	std::cout << (a <= b) << std::endl;
+	std::cout << (a == b) << std::endl;
+	std::cout << (a != b) << std::endl;
+	std::cout << (a + b).toFloat() << std::endl;
+	std::cout << (a - b).toFloat() << std::endl;
+	std::cout << (a * b).toFloat() << std::endl;
+	std::cout << (a / b).toFloat() << std::endl;
+	return (0);
 }
