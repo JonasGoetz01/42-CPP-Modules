ec1e5bd712a3b36de1aae7a35ed8177be75e087d
Jonas Götz <jonas.goetz01@web.de>
2024-05-23 18:15:03 +0200

    use attack from ScavTrap

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:03:38 by jgotz             #+#    #+#             */
-/*   Updated: 2024/05/23 16:45:47 by jgotz            ###   ########.fr       */
+/*   Updated: 2024/05/23 18:14:15 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,5 @@ void DiamondTrap::whoAmI()
 
 void DiamondTrap::attack(const std::string& target)
 {
-    if(this->energy_points > 0 && this->hit_points > 0)
-    {
-        std::cout << "DiamondTrap " << this->name << " attacks " << target << ", causing " << this->attack_damage << " points of damage!" << std::endl;
-        this->energy_points -= 1;
-    }
-    else
-        std::cout << "DiamondTrap " << this->name << " cannot attack!" << std::endl;
+    ScavTrap::attack(target);
 }
