c5b012c2a2500089d59519d0d92631157bc4d715
Jonas Götz <jonas.goetz01@web.de>
2024-05-24 21:24:00 +0200

    fix some

--- cpp04/ex03/ICharacter.hpp
+++ cpp04/ex03/ICharacter.hpp
@@ -1,19 +1,15 @@
 #pragma once
 
-#include "Materia.hpp"
+#include <string>
 
 class AMateria;
 
 class ICharacter
 {
-    protected:
-		const std::string _name;
-        
-	public:
-		virtual ~ICharacter() {}
-		virtual std::string const & getName() const = 0;
-		virtual void equip(AMateria *m) = 0;
-		virtual void unequip(int idx) = 0;
-		virtual void use(int idx, ICharacter& target) = 0;
+    public:
+        virtual ~ICharacter() {}
+        virtual std::string const & getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
 };
-
