0644cc216877f8e3247b30bd1a1b3158986aece6
Jonas Götz <jonas.goetz01@web.de>
2024-05-24 20:42:45 +0200

    fix character

--- cpp04/ex03/Character.hpp
+++ cpp04/ex03/Character.hpp
@@ -7,18 +7,20 @@
 
 class AMateria;
 
-class Character: public ICharacter
+class Character : public ICharacter
 {
     private:
-        std::string _name;
-        AMateria* _materia[4];
-    public:
-        Character(std::string name);
-        Character(const Character& other);
-        virtual ~Character();
-        Character& operator=(const Character& other);
-        std::string const & getName() const;
-        void equip(AMateria* m);
-        void unequip(int idx);
-        void use(int idx, ICharacter& target);
+		AMateria			*(_inventory[4]);
+		std::string const	_name;
+        
+	public:
+		Character(std::string name);
+		Character(Character const & ref);
+		~Character();
+		Character & operator=(Character const & ref);
+		std::string const & getName() const;
+		void equip(AMateria *m);
+		void unequip(int idx);
+		void use(int idx, ICharacter& target);
+		AMateria	*getMateriaFromInventory(int idx);
 };
